6856cde0c53f7cd1a7251c9d58c7a2ddd94145ec
Tony Stark <73957207+TonyStark-47@users.noreply.github.com>
2025-04-13 20:25:38 +0530

    database URI update

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///job.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', "sqlite:///blog.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
